54b10b6d8221a0f745336ca5d6beb0dfa4c823a9
Sera Hill <sahill1@buffs.wtamu.edu>
2024-04-06 09:44:07 -0500

    completed tutorial part 2

--- barky1/urls.py
+++ barky1/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from rest_framework import routers
 
 from barky1.quickstart import views
+from django.urls import path, include
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -29,5 +30,6 @@ router.register(r'groups', views.GroupViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include('snippets.urls')),
 ]
